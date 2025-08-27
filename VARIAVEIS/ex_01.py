68c232c5d31704bab84d5b065ce8bc4492654f13
Helder Paulino <helderpaulino44@gmail.com>
2025-08-27 20:50:34 -0300

    Sla

---
+++ VARIAVEIS/ex_01.py
@@ -0,0 +1,16 @@
+# Criação da primeira variável sem estabelecer o tipo
+
+nome = "Paulo"
+print(nome)
+# O código anterior tratou da criação de uma variável, onde a sua declaração (nome) recebeu como valor "Paulo"
+
+# Criação de uma variável que irá receber um valor numérico
+
+idade = 20
+print(idade)
+
+# print(nome, idade)
+
+#imprimir variáveis usando fstring
+
+print(f"Seu nome é {nome} e você tem {idade} anos")
